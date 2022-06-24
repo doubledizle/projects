ece2d5f0e47416cdaf39b0818eb37af0fe4362ea
Donny Duncan <donnyduncan@gmail.com>
2022-06-23 22:54:55 -0500

    6/23/2022 - Initial upload of Full Stack Template app
    Signed-off-by: Donny Duncan <donnyduncan@gmail.com>

--- full-stack-template/server.js
+++ full-stack-template/server.js
@@ -1,37 +1,37 @@
-// Variables
-
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
 
+
 let db,
     dbConnectionString = process.env.DB_STRING,
     dbName = 'full_stack_template',
     collection
-  //--
-//--
-
-// DB Connection
-
+  
 MongoClient.connect(dbConnectionString)
   .then(client => {
     console.log('Connected to Database')
     db = client.db(dbName)
     collection = db.collection('items')
   })
-//--
-
-
-// Middlewares
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+app.use(cors())
 
-// Listener
+app.get('/', async (request, response) => {
+  try {
+      response.render('index.ejs')
+  } catch (error) {
+      response.status(500).send({message: error.message})
+  }
+})
+
+
 
 app.listen(process.env.PORT || PORT, ()=>{
   console.log(`The server is running on port ${process.env.PORT}`)
